ffebca4433ffd326664a477a022242558a50cc22
NikR62 <ryzhkow.n.a@yandex.ru>
2024-11-07 22:57:48 +0300

    Add files via upload

--- lesson 2.py
+++ lesson 2.py
@@ -3,6 +3,6 @@ number_of_hours= 1.5
 course_name = 'Python'
 time_per_tusk = 1.5/12
 print(f'Курс: {course_name}, '
-      f'всего задач: {homework}, '
+      f'всего задач:{homework}, '
       f'затрачено часов: {number_of_hours}, '
-      f'среднее время выполнения: {time_per_tusk} часа')
+      f'среднее время выполнения {time_per_tusk} часа.')
